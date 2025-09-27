0dcc4ba3d7510d8d2efb0f5247bf4f8f3a4e0fc0
Jheylon Silva <jheylon@gmail.com>
2025-09-27 17:19:33 -0300

    Create programa_4_4.py

---
+++ programa_4_4.py
@@ -0,0 +1,22 @@
+
+
+plano = input("Qual é o seu plano de celular? ")
+if plano == 'falapouco':
+    minutos_no_plano = 100
+    extra = 0.20
+    preço = 50
+if plano == 'falamuito':
+    minutos_no_plano = 500
+    extra = 0.15
+    preço = 99
+if plano != 'falapouco' and plano != 'falamuito':
+    print('Não conheço este plano')
+if plano == 'falapouco' or plano == 'falamuito':
+    minutos_consumidos = int(input('Quantos minutos você consumiu? '))
+    print('Vocẽ vai pagar: ')
+    print(f'Preço do plano R${preço:10.2f}')
+    suplemento = 0
+    if minutos_consumidos > minutos_no_plano:
+        suplemento = extra * (minutos_consumidos - minutos_no_plano)
+    print(f'Suplemento     R${suplemento:10.2f}')
+    print(f'Total          R${preço + suplemento:10.2f} ')
