eca52763870eabb09c4edf1dc6644527f67e4b2b
Jheylon Silva <jheylon@gmail.com>
2025-09-28 11:35:43 -0300

    28-09

---
+++ exercicio_4_6.py
@@ -0,0 +1,11 @@
+
+pc_1 = 0.50
+pc_2 = 0.45
+distancia = int(input('Distância a percorrer: '))
+if distancia < 200:
+    preco = distancia * pc_1
+    print(f'A distância percorreda foi de {distancia}km e o valor a pagar é de R${preco:5.2f}')
+else:
+    preco = distancia * pc_2
+    print(f'A distância percorreda foi de {distancia}km e o valor a pagar é de R${preco:5.2f}')
+
